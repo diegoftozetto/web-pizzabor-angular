utfpr-cefwm/angular-aula03

Cria workspace Nx.
- Comando:
  npx create-nx-workspace --directory=.

✔ Workspace name (e.g., org name)     · artigaria
✔ What to create in the new workspace · angular
✔ Application name                    · web
✔ Default stylesheet format           · css
✔ Use Nx Cloud? (It's free and doesn't require registration.) · No

apps/web/src/app/app.component.ts
@@ -1,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'artigaria-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'web';
+}

diegoftozetto/web-pizzabor-angular

apps/web/src/app/app.component.ts
@@ -1,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pizzabor-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'web';
+}