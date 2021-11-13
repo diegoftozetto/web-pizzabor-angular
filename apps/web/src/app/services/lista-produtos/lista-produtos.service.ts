6753c19d25542e87d470c3a9a72632f8d716d03b
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 03:17:19 +0000

    Adiciona `ListaProdutosService.getAll`

--- apps/web/src/app/services/lista-produtos/lista-produtos.service.ts
+++ apps/web/src/app/services/lista-produtos/lista-produtos.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {
+  map,
+} from 'rxjs/operators';
+
+import {
+  Produto,
+  IProduto,
+} from '@pizzabor/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaProdutosService {
 
-  constructor() { }
+  constructor(
+    @Inject('API_BASE_URL') private apiBaseUrl: string,
+    private httpClient: HttpClient,
+  ) {
+  }
+
+  public getAll(): Observable<Produto[]> {
+    return this.httpClient.get<IProduto[]>(`${this.apiBaseUrl}/produtos`).pipe(
+      map((iProdutos: IProduto[]) => {
+        return iProdutos.map((iProduto: IProduto) => new Produto(
+          iProduto._id,
+          iProduto.imagem,
+          iProduto.nome,
+          iProduto.categoria,
+          iProduto.tamanho,
+          iProduto.preco,
+          iProduto.descricao,
+        ));
+      }),
+    );
+  }
 }
