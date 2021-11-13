1127edb7cf10fade5289b887aad873ed768f4e0c
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 06:37:26 +0000

    Chama formulário de login com erro 401

--- apps/web/src/app/modules/auth/auth.module.ts
+++ apps/web/src/app/modules/auth/auth.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,10 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
   ]
 })
 export class AuthModule { }
--- apps/web/src/app/modules/auth/components/login/login.component.ts
+++ apps/web/src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
+
+import { of } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+
+import { MessageService } from 'primeng/api';
+
+import { AuthResult, IUsuario } from '@pizzabor/common';
+
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'pizzabor-login',
@@ -7,7 +29,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup = this.formBuilder.group({
+    login: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private messageService: MessageService,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
+  public fazerLogin(): void {
+    const body: Pick<IUsuario, 'login' | 'senha'> = this.formGroup.value;
+    this.authService.login(body).pipe(
+      take(1),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.messageService.add({
+            severity: 'warn',
+            summary: 'Usuário ou senha inválidos',
+          });
+          return of(undefined);
+        }
+        throw err;
+      }),
+    ).subscribe((result?: AuthResult) => {
+      if (result?.jwt) {
+        this.messageService.add({
+          severity: 'success',
+          summary: `Olá, ${result.usuario.nome}!`,
+          detail: result.usuario.administrador
+            ? 'Você é administrador!'
+            : 'Estamos redirecionando você para o conteúdo...',
+        });
+        window.localStorage.setItem('jwt', result.jwt);
+        setTimeout(() => {
+          this.router.navigate(['/area-restrita']);
+        }, 1000);
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
