d1cc2a862d5220e836051033ce987551af6d649c
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 04:09:36 +0000

    Gera `ProdutoComponent`

--- apps/web/src/app/modules/produto-edicao/produto-edicao.module.ts
+++ apps/web/src/app/modules/produto-edicao/produto-edicao.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProdutoEdicaoRoutingModule } from './produto-edicao-routing.module';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProdutoComponent
+  ],
   imports: [
     CommonModule,
     ProdutoEdicaoRoutingModule
