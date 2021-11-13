c32994fb38c4c542b10998d7cd47540f28f779b2
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 01:13:33 +0000

    Instala o client do MongoDB no servidor Express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,15 @@
 
 import * as express from 'express';
 
+import { MongoClient } from 'mongodb';
+
+MongoClient.connect(
+  'mongodb://web-pizzabor-angular_devcontainer_db_1:27017',
+).then((client: MongoClient) => {
+  app.locals.db = client.db('pizzabor');
+  console.log('Conectado ao MongoDB.');
+});
+
 const app = express();
 
 app.get('/api', (req, res) => {
