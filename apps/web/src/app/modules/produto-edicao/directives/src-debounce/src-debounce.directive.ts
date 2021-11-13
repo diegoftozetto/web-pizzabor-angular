49ed2ec6a4c57bbc3aa02e7ab6132c063604b9e3
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 06:54:34 +0000

    Finaliza a implementação da diretiva customizada

--- apps/web/src/app/modules/produto-edicao/directives/src-debounce/src-debounce.directive.ts
+++ apps/web/src/app/modules/produto-edicao/directives/src-debounce/src-debounce.directive.ts
@@ -1,20 +1,53 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[pizzaborSrcDebounce]'
 })
-export class SrcDebounceDirective {
+export class SrcDebounceDirective implements OnChanges, OnDestroy {
+
+  private srcDebounced: string = '';
+
+  private src$: Subject<string> = new Subject();
+
+  private subUnsubscribe: Subject<void> = new Subject();
 
   @Input()
   public pizzaborSrcDebounce?: string;
 
   constructor(
   ) {
+    this.src$.pipe(
+      takeUntil(this.subUnsubscribe),
+      debounceTime(500),
+    ).subscribe(
+      (src: string) => this.srcDebounced = src,
+    );
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.pizzaborSrcDebounce) {
+      this.src$.next(this.pizzaborSrcDebounce);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.subUnsubscribe.next();
+    this.subUnsubscribe.complete();
   }
 
   @HostBinding('attr.src')
   public get src(): string {
-    return this.pizzaborSrcDebounce || '';
+    return this.srcDebounced;
   }
 
 }
