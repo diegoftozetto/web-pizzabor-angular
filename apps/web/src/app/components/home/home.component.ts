e15ac085f533426092e9876e0ad7e7af2d4a3328
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 16:38:22 +0000

    Adiciona opção de voltar para a página `área restrita` e `home logado` sem fazer login quando já existe jwt

--- apps/web/src/app/components/home/home.component.ts
+++ apps/web/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { IUsuario } from '@pizzabor/common';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -12,6 +13,50 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const jwt: (string | null) = window.localStorage.getItem('jwt');
+
+    if(jwt) {
+      const jwtHelper: JwtHelperService = new JwtHelperService();
+      const iUsuario: IUsuario =  jwtHelper.decodeToken(jwt);
+
+      if(iUsuario.administrador) {
+        this.items = [
+          {
+            label: 'Cardápio',
+            icon: 'pi pi-book',
+            routerLink: [ '/home' ],
+          },
+          {
+            label: 'Sobre',
+            icon: 'pi pi-info-circle',
+            routerLink: [ '/home/sobre' ],
+          },
+          {
+            label: 'Área Restrita',
+            icon: 'pi pi-user-edit',
+            routerLink: [ '/area-restrita' ],
+          },
+        ];
+      } else {
+        this.items = [
+          {
+            label: 'Cardápio',
+            icon: 'pi pi-book',
+            routerLink: [ '/home' ],
+          },
+          {
+            label: 'Sobre',
+            icon: 'pi pi-info-circle',
+            routerLink: [ '/home/sobre' ],
+          },
+          {
+            label: 'Lista de Compras',
+            icon: 'pi pi-shopping-cart',
+            routerLink: [ '/home-logado' ],
+          },
+        ];
+      }
+    }
   }
 
   public items: MenuItem[] = [
