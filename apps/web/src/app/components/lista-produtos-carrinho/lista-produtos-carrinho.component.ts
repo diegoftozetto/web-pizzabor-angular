3d3fafedf63f207b9d1bdeca6c661c2a0a3b87b9
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 16:22:47 +0000

    Adiciona `lista-produtos-carrinho` hardcode

--- apps/web/src/app/components/lista-produtos-carrinho/lista-produtos-carrinho.component.ts
+++ apps/web/src/app/components/lista-produtos-carrinho/lista-produtos-carrinho.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProdutosCarrinhoComponent implements OnInit {
 
-  constructor() { }
+  public produtosCarrinho: any[];
+
+  constructor(
+  ) {
+    this.produtosCarrinho = [
+      {nome: "Pizza Portuguesa", imagem: "https://images2.imgbox.com/7a/01/eNA5G8dJ_o.png", preco: 49.60, categoria: "pizza_salgada", tamanho: "Grande", quantidade: 1},
+      {nome: "Pizza Sensação", imagem: "https://images2.imgbox.com/a0/52/emVpeCWE_o.png", preco: 35.60, categoria: "pizza_doce", tamanho: "Média", quantidade: 1},
+      {nome: "Coca-Cola", imagem: "https://images2.imgbox.com/a5/a4/9yD9YYxw_o.jpg", preco: 5.50, categoria: "bebida", tamanho: "350ml", quantidade: 2},
+    ]
+  }
 
   ngOnInit(): void {
   }
