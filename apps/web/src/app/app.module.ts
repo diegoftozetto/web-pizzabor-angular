3031be23ec4886b1f129a3bc1e16a0212dd190ab
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 02:24:27 +0000

    Adiciona `CardapioService.getAll`

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import {MenubarModule} from 'primeng/menubar';
 
@@ -18,6 +19,7 @@ import { CardapioComponent } from './components/cardapio/cardapio.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     MenubarModule,
   ],
--- apps/web/src/app/services/cardapio/cardapio.service.ts
+++ apps/web/src/app/services/cardapio/cardapio.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {
+  map,
+} from 'rxjs/operators';
+
+import {
+  Produto,
+  IProduto,
+} from '@pizzabor/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardapioService {
 
-  constructor() { }
+  constructor(
+    @Inject('API_BASE_URL') private apiBaseUrl: string,
+    private httpClient: HttpClient,
+  ) {
+  }
+
+  public getAll(): Observable<Produto[]> {
+    return this.httpClient.get<IProduto[]>(`${this.apiBaseUrl}/cardapio`).pipe(
+      map((iProdutos: IProduto[]) => {
+        return iProdutos.map((iProduto: IProduto) => new Produto(
+          iProduto._id,
+          iProduto.imagem,
+          iProduto.nome,
+          iProduto.categoria,
+          iProduto.tamanho,
+          iProduto.preco,
+          iProduto.descricao,
+        ));
+      }),
+    );
+  }
 }
