8d59206e45d8290257ef3d96c844c7e6314ae5a7
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 04:07:35 +0000

    Gera `ProdutoEdicaoModule`

---
+++ apps/web/src/app/modules/produto-edicao/produto-edicao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdutoEdicaoRoutingModule } from './produto-edicao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProdutoEdicaoRoutingModule
+  ]
+})
+export class ProdutoEdicaoModule { }
