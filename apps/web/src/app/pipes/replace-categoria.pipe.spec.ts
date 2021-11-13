03e429c8c79ca87de8f94c36bc7b01cd5fb131ae
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 03:59:14 +0000

    Adiciona pipe customizada

---
+++ apps/web/src/app/pipes/replace-categoria.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ReplaceCategoriaPipe } from './replace-categoria.pipe';
+
+describe('ReplaceCategoriaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ReplaceCategoriaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ apps/web/src/app/pipes/replace-categoria.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'replaceCategoria'
+})
+export class ReplaceCategoriaPipe implements PipeTransform {
+
+  transform(
+    value: string,
+  ): string {
+    if (value === 'pizza_salgada') {
+      return 'Pizza Salgada';
+    } else if (value === 'massa') {
+      return 'Massa';
+    } else if (value === 'pizza_doce') {
+      return 'Pizza Doce'
+    }  else if (value === 'bebida') {
+      return 'Bebida'
+    }
+    return 'Outro';
+  }
+}
