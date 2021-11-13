8fea4a77f7e692ad6b2c2566209af1ed03486a5d
Diego Tozetto <diegotozetto@gmail.com>
2021-11-13 00:43:50 +0000

    Gera `CardapioCOmponent`

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -7,11 +7,13 @@ import {MenubarModule} from 'primeng/menubar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardapioComponent } from './components/cardapio/cardapio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CardapioComponent,
   ],
   imports: [
     BrowserModule,
---
+++ apps/web/src/app/components/cardapio/cardapio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pizzabor-cardapio',
+  templateUrl: './cardapio.component.html',
+  styleUrls: ['./cardapio.component.css']
+})
+export class CardapioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
